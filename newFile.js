b3549a94e413c69d192a8fecb9f7ad442d27b236
ambleandro <ambleandro@gmail.com>
2024-06-11 23:02:40 -0300

    :tada: initial commit

---
+++ newFile.js
@@ -0,0 +1,7 @@
+// substituir a imagem
+if (html.classList.contains("light")) {
+  img.setAttribute("src", "assets/avatar-light.png")
+  img.setAttribute("alt", "Foto do Leandro Ambrósio de óculos e com headset")
+} else {
+  img.setAttribute("src", "assets/avatar.png")
+}
