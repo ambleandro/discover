aa5852e925d1f67058cff77ee50f2f0f47ef95d7
ambleandro <ambleandro@gmail.com>
2024-06-12 11:57:45 -0300

    delete newFile.js

--- newFile.js
+++
@@ -1,7 +0,0 @@
-// substituir a imagem
-if (html.classList.contains("light")) {
-  img.setAttribute("src", "assets/avatar-light.png")
-  img.setAttribute("alt", "Foto do Leandro Ambrósio de óculos e com headset")
-} else {
-  img.setAttribute("src", "assets/avatar.png")
-}
